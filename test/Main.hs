d208f4301a769b3bf65b1e9450fd00283453f605
Arjun Kathuria <arjun.kathuria8@gmail.com>
2023-06-08 17:30:33 +0530

    Updates test-suite to skip undefined test
    * A test had it's definition as "undefined" and thus blew
      up when running tests. This commit comments it out.

--- test/Main.hs
+++ test/Main.hs
@@ -13,12 +13,12 @@
 
 module Main where
 
-import qualified EventSpec
+-- import qualified EventSpec
 import qualified MetricApiSpec
 import Test.Hspec (describe, hspec)
 
 
 main :: IO ()
 main = hspec $ do
-  describe "Event" EventSpec.spec
+  -- describe "Event" EventSpec.spec
   describe "MetricApi" MetricApiSpec.spec
