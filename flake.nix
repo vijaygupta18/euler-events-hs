362a1bd62c61a4d6d6191e1342d2a5563cec8ed4
Vijay Gupta <vijay.gupta@juspay.in>
2023-07-06 17:41:13 +0530

    updated with latest haskell flake

--- flake.nix
+++ flake.nix
@@ -2,9 +2,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    haskell-flake.url = "github:srid/haskell-flake/0.3.0";
-    prometheus-haskell.url = "github:juspay/prometheus-haskell/54b8fbd57257dbec0b4b76814773c5cebfae2def";
-    prometheus-haskell.flake = false;
+    haskell-flake.url = "github:srid/haskell-flake/908a59167f78035a123ab71ed77af79bed519771";
+    # prometheus-haskell.url = "github:juspay/prometheus-haskell/54b8fbd57257dbec0b4b76814773c5cebfae2def";
+    # prometheus-haskell.flake = false;
+    prometheus-haskell.url = "github:juspay/prometheus-haskell/more-proc-metrics";
+    prometheus-haskell.inputs.haskell-flake.follows = "haskell-flake";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } ({ withSystem, ... }: {
@@ -15,20 +17,18 @@
       perSystem = { self', pkgs, lib, config, ... }: {
         packages.default = self'.packages.euler-events-hs;
         haskellProjects.default = {
-          source-overrides = {
-            prometheus-client = inputs.prometheus-haskell + /prometheus-client;
-            prometheus-proc = inputs.prometheus-haskell + /prometheus-proc;
-            prometheus-metrics-ghc = inputs.prometheus-haskell + /prometheus-metrics-ghc;
-            wai-middleware-prometheus = inputs.prometheus-haskell + /wai-middleware-prometheus;
-          };
-
-          overrides = self: super:
-            with pkgs.haskell.lib.compose;
-            lib.mapAttrs (k: v: lib.pipe super.${k} v) {
-              prometheus-client = [ dontCheck ];
-              prometheus-proc = [ doJailbreak ];
-              euler-events-hs = [ doJailbreak dontCheck ];
+          imports = [
+            inputs.prometheus-haskell.haskellFlakeProjectModules.output
+          ];
+          packages = {};
+          settings = {
+            prometheus-client.check = false;
+            prometheus-proc.jailbreak = true;
+            euler-events-hs = {
+              check = false;
+              jailbreak = true;
             };
+          };
         };
       };
     });
